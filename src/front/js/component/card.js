5f056923fe22d637153d535832918b52f0554f47
MaxiZet <98966844+MaxiZet@users.noreply.github.com>
2024-11-29 23:52:14 +0000

    Se agregaron ajustes en las Cards/Menu

--- src/front/js/component/card.js
+++ src/front/js/component/card.js
@@ -2,12 +2,15 @@ import React from "react";
 
 const Card = ({ item }) => {
     return (
-        <div className="card" style={{ width: "12rem" }}>
-            <img src={item.image} className="card-img-top" alt="..."/>
+        <div className="card" style={{ width: "15rem", height: "20rem" }}>
+            <img src={item.image} className="card-img-top" alt="..." style={{ height: "10rem" }} />
             <div className="card-body">
-                <p className="card-text">{item.title}</p>
+                <h5 className="card-title">{item.title}</h5>
+                <p className="card-text">{item.description}</p>
             </div>
+            <button href="#" className="btn btn-primary">Reservar</button>
         </div>
+        
     );
 };
 
--- src/front/js/pages/menu.js
+++ src/front/js/pages/menu.js
@@ -7,12 +7,32 @@ export const Menu = () => {
     const { store } = useContext(Context); // Accessing store from Flux context
 
     return (
-        <div className="container">
-            <div className="row">
+        <div className="container mt-3">
+            <div className="row text-center">
+                <h2 className="fs-1">Menus semanales</h2>
                 {/* Map through store.menu array and create Card components */}
                 {store.menu.map((item) => (
                     <div className="col-12 col-sm-6 col-md-4 mb-4" key={item.id}>
                         <Card item={item} />
+                    </div>
+                ))}
+            </div>
+            <div className="row text-center">
+                <h2 className="fs-1">Minutas</h2>
+                {/* Map through store.menu array and create Card components */}
+                {store.menu.map((item) => (
+                    <div className="col-12 col-sm-6 col-md-4 mb-4" key={item.id}>
+                        <Card item={item} />
+                    </div>
+                ))}
+            </div>
+            <div className="row text-center">
+                <h2 className="fs-1">Bebidas</h2>
+                {/* Map through store.menu array and create Card components */}
+                {store.bebidas.map((item) => (
+                    <div className="col-12 col-sm-6 col-md-4 mb-4" key={item.id}>
+                        <Card item={item}/>
+                        
                     </div>
                 ))}
             </div>
