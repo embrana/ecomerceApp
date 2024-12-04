5857899b53a08d5502e24c297627826d8023f3fb
Elibar55 <87397616+Elibar55@users.noreply.github.com>
2024-12-04 21:26:24 +0000

    started working with dashboard cocina

--- src/front/js/pages/dashboardCocina.js
+++ src/front/js/pages/dashboardCocina.js
@@ -1,16 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const DashboardCocina = () => {
     return (
-
         <div className="container mt-5">
             <h1 className="mb-2">Listado De Menus</h1>
-            <div className="d-flex justify-content-between mt-4 p-5d">
+            <div className="d-flex justify-content-between mt-4 p-5">
                 <div className="d-flex">
-                    <button className="btn btn-secondary dropdown-toggle me-2 " type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <button className="btn btn-secondary dropdown-toggle me-2" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                         Action
                     </button>
-                    <ul className="dropdown-menu ">
+                    <ul className="dropdown-menu">
                         <li><a className="dropdown-item" href="#">Action</a></li>
                         <li><a className="dropdown-item" href="#">Another action</a></li>
                         <li><a className="dropdown-item" href="#">Something else here</a></li>
@@ -21,17 +21,21 @@ const DashboardCocina = () => {
                     </form>
                 </div>
                 <div>
-                    <a className="btn btn-primary ustify-content-end!" href="https://fluffy-space-sniffle-v6gw54q4vvxg3x5gx-3000.app.github.dev/add/menu" role="button">Añadir Menu</a>
+                    <Link to="/add/menu">
+                        <button className="btn btn-primary" role="button">Añadir Menu</button>
+                    </Link>
                 </div>
             </div>
             <div>
-                <table className="table table-secondary-emphasis table-striped mt-3 .bg-secundario">
+                <table className="table table-secondary table-striped mt-3">
                     <thead>
                         <tr>
-                            <th scope="col"><div className="form-check">
-                                <input className="form-check-input" type="checkbox" value="" id="flexCheckIndeterminate" />
-                                <label className="form-check-label" for="flexCheckIndeterminate" />
-                            </div></th>
+                            <th scope="col">
+                                <div className="form-check">
+                                    <input className="form-check-input" type="checkbox" id="flexCheckIndeterminate" />
+                                    <label className="form-check-label" htmlFor="flexCheckIndeterminate"></label>
+                                </div>
+                            </th>
                             <th scope="col">Fecha</th>
                             <th scope="col">Tipo</th>
                             <th scope="col">Descripcion</th>
@@ -41,44 +45,45 @@ const DashboardCocina = () => {
                             <th scope="col">Imagen</th>
                             <th scope="col">Precio</th>
                             <th scope="col"></th>
-
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
-                            <th scope="row"><div className="form-check">
-                                <input className="form-check-input" type="checkbox" value="" id="flexCheckIndeterminate" />
-                                <label className="form-check-label" for="flexCheckIndeterminate" />
-                            </div></th>
+                            <th scope="row">
+                                <div className="form-check">
+                                    <input className="form-check-input" type="checkbox" id="flexCheckIndeterminateRow" />
+                                    <label className="form-check-label" htmlFor="flexCheckIndeterminateRow"></label>
+                                </div>
+                            </th>
                             <td>01/06/23</td>
                             <td>Vegetariano</td>
                             <td>Milanesa de Seitan y Guarnicion</td>
                             <td>Activo</td>
                             <td>Menu Ejecutivo</td>
                             <td>150</td>
-                            <td>
-                                <i className="fa-solid fa-image"></i></td>
+                            <td><i className="fa-solid fa-image"></i></td>
                             <td>$150</td>
-                            <td className="d-flez justifyd-flex justify-content-end">
-                                <a className="btn ustify-content-end!" href="https://fluffy-space-sniffle-v6gw54q4vvxg3x5gx-3000.app.github.dev/add/menu" role="button"> <i className="fa-solid fa-pen"></i></a>
-                                <button className="btn dropdown-toggle me-2 " type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    <i className="fa-solid fa-ellipsis m-auto"></i>
+                            <td className="d-flex justify-content-end">
+                                <Link to="/add/menu" className="btn">
+                                    <i className="fa-solid fa-pen"></i>
+                                </Link>
+                                <button className="btn dropdown-toggle me-2" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <i className="fa-solid fa-ellipsis"></i>
                                 </button>
-                                <ul className="dropdown-menu ">
+                                <ul className="dropdown-menu">
                                     <li><a className="dropdown-item" href="#">Activo</a></li>
                                     <li><a className="dropdown-item" href="#">Inactivo</a></li>
                                 </ul>
-                                {/* <!-- Button trigger modal --> */}
                                 <button type="button" className="btn" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                                <i className="fa-solid fa-trash"></i>
+                                    <i className="fa-solid fa-trash"></i>
                                 </button>
 
-                                {/* modal */}
-                                <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                {/* Modal */}
+                                <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                                     <div className="modal-dialog">
                                         <div className="modal-content">
                                             <div className="modal-header">
-                                                <h1 className="modal-title fs-5" id="exampleModalLabel">¿usted esta seguro que desea eliminar de forma permanente?</h1>
+                                                <h1 className="modal-title fs-5" id="exampleModalLabel">¿Está seguro que desea eliminar este menú de forma permanente?</h1>
                                                 <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                             </div>
                                             <div className="modal-footer">
@@ -89,9 +94,7 @@ const DashboardCocina = () => {
                                     </div>
                                 </div>
                             </td>
-
                         </tr>
-
                     </tbody>
                 </table>
             </div>
@@ -99,22 +102,21 @@ const DashboardCocina = () => {
                 <div className="d-flex">
                     <select className="form-select me-2" aria-label="Default select example">
                         <option selected>1</option>
-                        <option value="1">2</option>
-                        <option value="2">3</option>
-                        <option value="3">4</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
                     </select>
                 </div>
-                <p className="d-flex mx-4 "> 1-10 of 10</p>
-                <div className="d-felx">
-                    <button className="btn btn-primary justify-content-end! me-2" type="submit"><i className="fa-solid fa-angles-left"></i></button>
-                    <button className="btn btn-primary justify-content-end! me-2" type="submit"><i className="fa-solid fa-angle-left"></i></button>
-                    <button className="btn btn-primary justify-content-end! me-2" type="submit"><i className="fa-solid fa-angle-right"></i></button>
-                    <button className="btn btn-primary justify-content-end!" type="submit"><i className="fa-solid fa-angles-right"></i></button>
+                <p className="mx-4">1-10 of 10</p>
+                <div className="d-flex">
+                    <button className="btn btn-primary me-2" type="button"><i className="fa-solid fa-angles-left"></i></button>
+                    <button className="btn btn-primary me-2" type="button"><i className="fa-solid fa-angle-left"></i></button>
+                    <button className="btn btn-primary me-2" type="button"><i className="fa-solid fa-angle-right"></i></button>
+                    <button className="btn btn-primary" type="button"><i className="fa-solid fa-angles-right"></i></button>
                 </div>
             </div>
-        </div >
-
-    )
+        </div>
+    );
 };
 
 export default DashboardCocina;
