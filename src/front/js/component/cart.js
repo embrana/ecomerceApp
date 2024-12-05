b7c757118b0f7f9bf7cc0aa4e07e63f93c06d7de
MaxiZet <98966844+MaxiZet@users.noreply.github.com>
2024-12-05 23:08:36 +0000

    Se ajusto el carrito, se sacaron las imagenes y se alinearon los items

--- src/front/js/component/cart.js
+++ src/front/js/component/cart.js
@@ -26,84 +26,72 @@ const Cart = () => {
                             <div className="panel-title">
                                 <div className="row">
                                     <div className="col-6">
-                                        <h5><span className="glyphicon glyphicon-shopping-cart"></span> Shopping Cart</h5>
-                                    </div>
-                                    <div className="col-6">
-                                        <button type="button" className="btn btn-primary btn-sm btn-block">
-                                            <span className="glyphicon glyphicon-share-alt"></span> Continue shopping
-                                        </button>
+                                        <p className="fs-4 fw-bold"><i class="fa-solid fa-cart-shopping"></i> Shopping Cart</p>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        
+
                         <div className="panel-body mt-2">
                             <div className="row">
-                                <div className="col-2"><img className="img-responsive" src="http://placehold.it/100x70"/>
-                                </div>
                                 <div className="col-4">
-                                    <h4 className="product-name"><strong>Product name</strong></h4><h4><small>Product description</small></h4>
+                                    <p className="product-name fs-4">Product name</p>
                                 </div>
-                                <div className="col-6">
-                                    <div className="col-6 text-right">
-                                        <h6><strong>25.00 <span className="text-muted">x</span></strong></h6>
+                                <div className="col-6 row">
+                                    <div className="col-6 text-end">
+                                        <p className="fs-5 text">$ 25.00 <span className="text-muted">x</span></p>
                                     </div>
                                     <div className="col-4">
                                         <input type="text" className="form-control input-sm" value="1" />
                                     </div>
                                     <div className="col-2">
                                         <button type="button" className="btn btn-link btn-xs">
-                                            <span className="glyphicon glyphicon-trash"> </span>
+                                            <i class="fa-solid fa-trash"></i>
                                         </button>
                                     </div>
                                 </div>
                             </div>
                             <div className="row">
-                                <div className="col-2"><img className="img-responsive" src="http://placehold.it/100x70"/>
-                                </div>
                                 <div className="col-4">
-                                    <h4 className="product-name"><strong>Product name</strong></h4><h4><small>Product description</small></h4>
+                                    <p className="product-name fs-4">Milanesa con pure</p>
                                 </div>
-                                <div className="col-6">
-                                    <div className="col-6 text-right">
-                                        <h6><strong>25.00 <span className="text-muted">x</span></strong></h6>
+                                <div className="col-6 row">
+                                    <div className="col-6 text-end">
+                                        <p className="fs-5 text">$ 25.00 <span className="text-muted">x</span></p>
                                     </div>
                                     <div className="col-4">
                                         <input type="text" className="form-control input-sm" value="1" />
                                     </div>
                                     <div className="col-2">
                                         <button type="button" className="btn btn-link btn-xs">
-                                            <span className="glyphicon glyphicon-trash"> </span>
+                                            <i class="fa-solid fa-trash"></i>
                                         </button>
                                     </div>
                                 </div>
                             </div>
                             <div className="row">
-                                <div className="col-2"><img className="img-responsive" src="http://placehold.it/100x70"/>
-                                </div>
                                 <div className="col-4">
-                                    <h4 className="product-name"><strong>Product name</strong></h4><h4><small>Product description</small></h4>
+                                    <p className="product-name fs-4">Coca cola</p>
                                 </div>
-                                <div className="col-6">
-                                    <div className="col-6 text-right">
-                                        <h6><strong>25.00 <span className="text-muted">x</span></strong></h6>
+                                <div className="col-6 row">
+                                    <div className="col-6 text-end">
+                                        <p className="fs-5 text">$ 25.00 <span className="text-muted">x</span></p>
                                     </div>
                                     <div className="col-4">
                                         <input type="text" className="form-control input-sm" value="1" />
                                     </div>
                                     <div className="col-2">
                                         <button type="button" className="btn btn-link btn-xs">
-                                            <span className="glyphicon glyphicon-trash"> </span>
+                                            <i class="fa-solid fa-trash"></i>
                                         </button>
                                     </div>
                                 </div>
                             </div>
-
                         </div>
                         <div className="panel-footer mb-2">
                             <div className="row text-center">
                                 <div className="col-9">
-                                    <h4 className="text-right">Total <strong>$50.00</strong></h4>
+                                    <h4 className="text-right">Total <strong>$ 75.00</strong></h4>
                                 </div>
                                 <div className="col-3">
                                     <button type="button" className="btn btn-success btn-block">
