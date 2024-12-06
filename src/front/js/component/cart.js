28a8e7e5b4229b65bff6428ffffc991be5f272cb
embrana <67813424+embrana@users.noreply.github.com>
2024-12-06 01:15:15 +0000

    minor fixex in cart component due merge issues

--- src/front/js/component/cart.js
+++ src/front/js/component/cart.js
@@ -48,54 +48,13 @@ const Cart = () => {
                                     </div>
                                 </div>
                             </div>
-                            <div className="row">
-                                <div className="col-4">
-                                    <p className="product-name fs-4">Milanesa con pure</p>
-                                </div>
-                                <div className="col-6 row">
-                                    <div className="col-6 text-end">
-                                        <p className="fs-5 text">$ 25.00 <span className="text-muted">x</span></p>
-                                    </div>
-                                    <div className="col-4">
-                                        <input type="text" className="form-control input-sm" value="1" />
-                                    </div>
-                                    <div className="col-2">
-                                        <button type="button" className="btn btn-link btn-xs">
-                                            <i class="fa-solid fa-trash"></i>
-                                        </button>
-                                    </div>
-                                </div>
                             </div>
-                            <div className="row">
-                                <div className="col-4">
-                                    <p className="product-name fs-4">Coca cola</p>
-                                </div>
-                                <div className="col-6 row">
-                                    <div className="col-6 text-end">
-                                        <p className="fs-5 text">$ 25.00 <span className="text-muted">x</span></p>
-                                    </div>
-                                    <div className="col-4">
-                                        <input type="text" className="form-control input-sm" value="1" />
-                                    </div>
-                                    <div className="col-2">
-                                        <button type="button" className="btn btn-link btn-xs"  onClick={() => actions.removeFromCart(index)} >
-                                            <span className="glyphicon glyphicon-trash"> </span>
-                                        </button>
-                                    </div>
-                                </div>
-                            </div> 
+                            </div>
                        
                     ))
                 ) : (
                     <li className="dropdown-item text-muted">Your cart is empty</li>
                 )}
-                                        <button type="button" className="btn btn-link btn-xs">
-                                            <i class="fa-solid fa-trash"></i>
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
                         <div className="panel-footer mb-2">
                             <div className="row text-center">
                                 <div className="col-9">
@@ -111,6 +70,7 @@ const Cart = () => {
                     </div>
                 </div>
             </div>
+        </div>
         </div>
     );
 };
