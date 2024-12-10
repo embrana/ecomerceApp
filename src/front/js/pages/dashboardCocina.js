e91700f56dd12d9c57dd3e7da3e769dccd5335fb
Elibar55 <87397616+Elibar55@users.noreply.github.com>
2024-12-10 16:01:21 +0000

    changed models.py in order to change orders containing several products, also changed dashboardCocina in order to consume this new data

--- src/front/js/pages/dashboardCocina.js
+++ src/front/js/pages/dashboardCocina.js
@@ -1,17 +1,16 @@
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
-import { Context } from "../store/appContext"; 
+import { Context } from "../store/appContext";
 
 const DashboardCocina = () => {
-  
   const { store, actions } = React.useContext(Context);
-  const { orders } = store;  // Access orders from the store
+  const { orders } = store; // Access orders from the store
 
   useEffect(() => {
-    
     actions.getOrders();
-  }, []);
+  }, [actions]);
 
+  console.log(store.orders);
 
   return (
     <div className="container mt-5">
@@ -61,9 +60,11 @@ const DashboardCocina = () => {
               Añadir Menu
             </button>
           </Link>
-          <Link to={"/menu"} className="ms-2">
-      <button type="submit" className="btn btn-primary">Compra Menu</button>
-      </Link>
+          <Link to="/menu" className="ms-2">
+            <button type="submit" className="btn btn-primary">
+              Compra Menu
+            </button>
+          </Link>
         </div>
       </div>
       <div>
@@ -101,60 +102,79 @@ const DashboardCocina = () => {
               </tr>
             ) : (
               orders.map((order, index) => (
-                <tr key={order.id || index}>
-                  <th scope="row">
-                    <div className="form-check">
-                      <input
-                        className="form-check-input"
-                        type="checkbox"
-                        id={`orderCheck${index}`}
-                      />
-                      <label
-                        className="form-check-label"
-                        htmlFor={`orderCheck${index}`}
-                      ></label>
-                    </div>
-                  </th>
-                  <td>{new Date(order.date).toLocaleDateString() || "N/A"}</td>
-                  <td>{order.product.type || "N/A"}</td>
-                  <td>{order.product.description || "N/A"}</td>
-                  <td>{order.status || "N/A"}</td>
-                  <td>{order.product.name || "N/A"}</td>
-                  <td>{order.product.stock || 0}</td>
-                  <td>
-                    <img
-                      src={order.product.image || "placeholder.jpg"}
-                      alt={order.product.name || "Product"}
-                      style={{ width: "50px" }}
-                    />
-                  </td>
-                  <td>{`$${order.product.price || 0}`}</td>
-                  <td className="d-flex justify-content-end">
-                    <Link to="/add/menu" className="btn">
-                      <i className="fa-solid fa-pen"></i>
-                    </Link>
-                    <button
-                      className="btn dropdown-toggle me-2"
-                      type="button"
-                      data-bs-toggle="dropdown"
-                      aria-expanded="false"
-                    >
-                      <i className="fa-solid fa-ellipsis"></i>
-                    </button>
-                    <ul className="dropdown-menu">
-                      <li>
-                        <a className="dropdown-item" href="#">
-                          Edit
-                        </a>
-                      </li>
-                      <li>
-                        <a className="dropdown-item" href="#">
-                          Delete
-                        </a>
-                      </li>
-                    </ul>
-                  </td>
-                </tr>
+                <React.Fragment key={order.id || index}>
+                  {/* Order Details Row */}
+                  <tr>
+                    <th scope="row">
+                      <div className="form-check">
+                        <input
+                          className="form-check-input"
+                          type="checkbox"
+                          id={`orderCheck${index}`}
+                        />
+                        <label
+                          className="form-check-label"
+                          htmlFor={`orderCheck${index}`}
+                        ></label>
+                      </div>
+                    </th>
+                    <td>{new Date(order.date).toLocaleDateString() || "N/A"}</td>
+                    <td>{order.order_number || "N/A"}</td>
+                    <td>{order.status || "N/A"}</td>
+                    <td colSpan="3">{order.status || "N/A"}</td>
+                    <td className="d-flex justify-content-end">
+                      <Link to="/add/menu" className="btn">
+                        <i className="fa-solid fa-pen"></i>
+                      </Link>
+                      <button
+                        className="btn dropdown-toggle me-2"
+                        type="button"
+                        data-bs-toggle="dropdown"
+                        aria-expanded="false"
+                      >
+                        <i className="fa-solid fa-ellipsis"></i>
+                      </button>
+                      <ul className="dropdown-menu">
+                        <li>
+                          <a className="dropdown-item" href="#">
+                            Edit
+                          </a>
+                        </li>
+                        <li>
+                          <a className="dropdown-item" href="#">
+                            Delete
+                          </a>
+                        </li>
+                      </ul>
+                    </td>
+                  </tr>
+
+                  {/* Products Rows (below the order) */}
+                  {order.products && order.products.length > 0 ? (
+                    order.products.map((orderProduct, productIndex) => (
+                      <tr key={productIndex}>
+                        <td colSpan="2"></td>
+                        <td>{orderProduct.name || "N/A"}</td>
+                        <td>{orderProduct.description || "N/A"}</td>
+                        <td>{orderProduct.status || "N/A"}</td>
+                        <td>{orderProduct.category || "N/A"}</td>
+                        <td>{orderProduct.stock || 0}</td>
+                        <td>
+                          <img
+                            src={orderProduct.image || "https://res.cloudinary.com/dnmm7omko/image/upload/v1733842727/ubstteb7dmizj50zozse.webp"}
+                            alt={orderProduct.name || "Product"}
+                            style={{ width: "50px" }}
+                          />
+                        </td>
+                        <td>{`$${orderProduct.price || 0}`}</td>
+                      </tr>
+                    ))
+                  ) : (
+                    <tr>
+                      <td colSpan="10">No products in this order</td>
+                    </tr>
+                  )}
+                </React.Fragment>
               ))
             )}
           </tbody>
